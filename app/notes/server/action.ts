0a375341729614a9ed3cccb10e66e5b0a356ce9a
pace11 <ryanjoker87@gmail.com>
2025-02-24 03:12:21 +0700

    feat: server action mutation app router

---
+++ app/notes/server/action.ts
@@ -0,0 +1,50 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+
+type FieldErrors = {
+  [key: string]: string[]
+}
+
+export async function createNote(
+  prevState: {
+    message: string
+    errors: object
+  },
+  formData: FormData,
+) {
+  const formSchema = z.object({
+    title: z.string().min(1, 'Title wajib diisi !'),
+    description: z.string().min(1, 'Description wajib diisi !'),
+  })
+
+  const parse = formSchema.safeParse({
+    title: formData.get('title'),
+    description: formData.get('description'),
+  })
+
+  if (!parse.success) {
+    const fieldErrors: FieldErrors = parse.error.formErrors.fieldErrors || {}
+    const errors = Object.keys(fieldErrors)?.reduce((acc, key) => {
+      acc[key] = fieldErrors[key]?.[0] || 'Unknown error'
+      return acc
+    }, {} as Record<string, string>)
+    return { errors }
+  }
+
+  try {
+    await fetch('https://service.pace-unv.cloud/api/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(parse.data),
+    }).then((res) => res.json())
+
+    revalidatePath('/notes/server')
+    return { message: 'Added notes successfully' }
+  } catch (error) {
+    return { message: 'Failed to create notes' }
+  }
+}
--- app/notes/server/page.tsx
+++ app/notes/server/page.tsx
@@ -1,3 +1,5 @@
+import CreateForm from './create'
+
 async function getNotes() {
   const notes = await fetch('https://service.pace-unv.cloud/api/notes').then(
     (res) => res.json(),
@@ -10,10 +12,18 @@ export default async function Notes() {
   const notes = await getNotes()
 
   return (
-    <ul>
-      {notes.data.map((el: { id: string; title: string }) => (
-        <li key={el.id}>{el.title}</li>
-      ))}
-    </ul>
+    <>
+      <CreateForm />
+      <div className="grid grid-cols-4 gap-4">
+        {notes.data.map(
+          (el: { id: string; title: string; description: string }) => (
+            <div key={el.id} className="p-4 bg-white shadow-sm rounded-lg">
+              <h1>{el.title}</h1>
+              <p>{el.description}</p>
+            </div>
+          ),
+        )}
+      </div>
+    </>
   )
 }
